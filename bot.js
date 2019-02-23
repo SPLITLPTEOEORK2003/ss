2914e40d9708ec5628067b55126666b7c4064206
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2019-02-24 00:16:03 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "#";
+
+
+
+client.on('message', message => {
+    if (message.content.startsWith("a")) {
+if(!message.channel.guild) return;
+        var mentionned = message.mentions.users.first();
+    var client;
+      if(mentionned){
+          var client = mentionned; } else {
+          var client = message.author;
+      }
+        const embed = new Discord.RichEmbed()
+                           .addField('Requested by:', "<@" + message.author.id + ">")
+        .setColor(000000)
+        .setImage(`${client.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
